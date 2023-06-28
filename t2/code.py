bfbd9cd2de9c6788833e7c71ecc97bc00dfae253
Leandro Furlam Turi <leandrofturi@gmail.com>
2023-06-28 20:01:59 -0300

    acabou

---
+++ t2/code.py
@@ -0,0 +1,336 @@
+import sys
+import time
+import random
+import pickle
+
+import numpy as np
+from datetime import timedelta
+# import matplotlib.pyplot as plt
+
+
+"""# Bee Colony"""
+
+# maximization problem
+
+def initialize(PS, bee_size):
+    beemin = -1
+    beemax = 1
+    hive = [beemin + np.random.random(bee_size)*(beemax - beemin) for _ in range(PS)]
+    return hive
+
+
+def evaluate(hive, fitness):
+    F = [{"X": bee, "F": fitness(bee)} for bee in hive]
+    return F
+
+
+def employed_bees(hive, fitness):
+    V = [bee["X"] + random.uniform(-1, 1)*(bee["X"] - random.choice(hive)["X"]) for bee in hive]
+    V = evaluate(V, fitness)
+    has_improved = False
+    E = []
+    for bee, v in zip(hive, V):
+        if v["F"] > bee["F"]:
+            E.append(v)
+            has_improved = True
+        else:
+            E.append(bee)
+    return E, has_improved
+
+
+def onlooker_bees(hive, fitness):
+    V = [bee["X"] + random.uniform(-1, 1)*(bee["X"] - random.choice(hive)["X"]) for bee in hive]
+    V = evaluate(V, fitness)
+    sum_fitness = sum([bee["F"] for bee in hive])
+    Pbees = [bee["F"]/sum_fitness for bee in hive]
+    sum_fitness = sum([v["F"] for v in V])
+    PV = [v["F"]/sum_fitness for v in V]
+    has_improved = False
+    O = []
+    for bee, v, pbee, pv in zip(hive, V, Pbees, PV):
+        if pv > pbee:
+            O.append(v)
+            has_improved = True
+        else:
+            O.append(bee)
+    return O, has_improved
+
+
+def scout_bees(bees, fitness):
+    _PS = len(bees)
+    _bee_size = len(bees[0]["X"])
+    S = initialize(_PS, _bee_size)
+    S = evaluate(S, fitness)
+    return S
+
+def ABC(X, Y, target_map, PS, bee_size, Lit, max_it, fitness):
+    K = len(target_map)
+    _fitness = lambda x: fitness(X, Y, target_map, K, x)
+    hive = initialize(PS, bee_size)
+    hive = evaluate(hive, _fitness)
+    gbest = hive[np.argmax([bee["F"] for bee in hive])]
+    history = []
+    n_iter = 1
+    Lit_count = 0
+    while (n_iter < max_it):
+        sys.stdout.write("\r%d" % n_iter)
+        sys.stdout.flush()
+
+        hive, employed_has_improved = employed_bees(hive, _fitness)
+        hive, onlooker_has_improved = onlooker_bees(hive, _fitness)
+        if (not employed_has_improved) and (not onlooker_has_improved):
+            Lit_count = Lit_count + 1
+        else:
+            Lit_count = 0
+        if Lit_count >= Lit:
+            hive = scout_bees(hive, _fitness)
+            Lit_count = 0
+        n_iter = n_iter + 1
+        best = hive[np.argmax([bee["F"] for bee in hive])]
+        if best["F"] > gbest["F"]:
+            gbest = best
+        history.append(gbest["F"])
+    sys.stdout.write("\n")
+    return gbest["X"], gbest["F"], history
+
+
+def fitness(X, Y, target_map, K, bee):
+    # Y_pred = []
+    # for row in X:
+    #     distances = [np.linalg.norm(row - centroid) for centroid in np.array_split(bee, K)]
+    #     Y_pred.append(target_map[np.argmax(distances)])
+    Y_pred = [target_map[np.argmax([np.linalg.norm(row - centroid) for centroid in np.array_split(bee, K)])] for row in X]
+    return sum(Y_pred == Y)/len(Y)
+
+
+"""# Train
+
+## Data
+"""
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+# data = datasets.load_breast_cancer()
+# data = datasets.load_diabetes()
+# data = datasets.load_wine()
+
+def prepare_dataset(data):
+    X = data["data"]
+    Y = data["target"]
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, 
+                                                        test_size=0.25, random_state=42)
+    # normalization
+    mu = np.mean(X_train)
+    sigma = np.std(X_train)
+    X_train_scl = [(x - mu)/sigma for x in X_train]
+    X_test_scl = [(x - mu)/sigma for x in X_test]
+
+    target_map = {i: y for i, y in zip(range(len(np.unique(Y))), np.unique(Y))}
+
+    return X_train_scl, X_test_scl, y_train, y_test, target_map, mu, sigma
+
+
+"""# Train
+
+### wine
+"""
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_wine())
+
+PS = 50
+Lit = 30
+max_it = 500
+K = len(target_map)
+bee_size = K * len(X[0])
+
+start = time.time()
+X_ABC, f_ABC, hist_ABC = ABC(X, Y, target_map, PS, bee_size, Lit, max_it, fitness)
+end = time.time()
+print(str(timedelta(seconds=end - start)))
+print(f_ABC)
+print(fitness(X_test, Y_test, target_map, K, X_ABC))
+
+file = open('data/wine.dat', 'wb')
+d = {"X_ABC": X_ABC, "f_ABC": f_ABC, "hist_ABC": hist_ABC, 
+     "X": X, "X_test": X_test, "Y": Y, "Y_test": Y_test, "target_map": target_map, "mu": mu, "sigma": sigma }
+pickle.dump(d, file)
+file.close()
+
+# plt.plot(range(len(hist_ABC)), hist_ABC)
+# plt.xlabel("iter")
+# plt.ylabel("acc")
+# plt.show()
+
+
+"""# Train
+
+### breast_cancer
+"""
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_breast_cancer())
+
+PS = 50
+Lit = 30
+max_it = 500
+K = len(target_map)
+bee_size = K * len(X[0])
+
+start = time.time()
+X_ABC, f_ABC, hist_ABC = ABC(X, Y, target_map, PS, bee_size, Lit, max_it, fitness)
+end = time.time()
+print(str(timedelta(seconds=end - start)))
+print(f_ABC)
+print(fitness(X_test, Y_test, target_map, K, X_ABC))
+
+file = open('data/breast_cancer.dat', 'wb')
+d = {"X_ABC": X_ABC, "f_ABC": f_ABC, "hist_ABC": hist_ABC, 
+     "X": X, "X_test": X_test, "Y": Y, "Y_test": Y_test, "target_map": target_map, "mu": mu, "sigma": sigma }
+pickle.dump(d, file)
+file.close()
+
+# plt.plot(range(len(hist_ABC)), hist_ABC)
+# plt.xlabel("iter")
+# plt.ylabel("acc")
+# plt.show()
+
+
+"""# Train
+
+### diabetes
+"""
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_diabetes())
+
+PS = 50
+Lit = 30
+max_it = 500
+K = len(target_map)
+bee_size = K * len(X[0])
+
+start = time.time()
+X_ABC, f_ABC, hist_ABC = ABC(X, Y, target_map, PS, bee_size, Lit, max_it, fitness)
+end = time.time()
+print(str(timedelta(seconds=end - start)))
+print(f_ABC)
+print(fitness(X_test, Y_test, target_map, K, X_ABC))
+
+file = open('data/diabetes.dat', 'wb')
+d = {"X_ABC": X_ABC, "f_ABC": f_ABC, "hist_ABC": hist_ABC, 
+     "X": X, "X_test": X_test, "Y": Y, "Y_test": Y_test, "target_map": target_map, "mu": mu, "sigma": sigma }
+pickle.dump(d, file)
+file.close()
+
+# plt.plot(range(len(hist_ABC)), hist_ABC)
+# plt.xlabel("iter")
+# plt.ylabel("acc")
+# plt.show()
+
+
+"""# Comparison"""
+
+
+import numpy as np
+from pymoo.core.problem import ElementwiseProblem
+
+
+class problem(ElementwiseProblem):
+    def __init__(self):
+        super().__init__(n_var=bee_size, n_obj=1, n_constr=0, xl=[-100 for _ in range(bee_size)], xu=[100 for _ in range(bee_size)])
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        bee = x
+        Y_pred = [target_map[np.argmax([np.linalg.norm(row - centroid) for centroid in np.array_split(bee, K)])] for row in X]
+        out["F"] = -sum(Y_pred == Y)/len(Y)
+
+
+"""# Comparison
+
+# GA
+"""
+
+
+from pymoo.optimize import minimize
+from pymoo.algorithms.soo.nonconvex.ga import GA
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_wine())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = GA(pop_size=50)
+d_wine = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_wine.F, len(d_wine.history))
+print(fitness(X_test, Y_test, target_map, K, d_wine.X))
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_breast_cancer())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = GA(pop_size=50)
+d_breast_cancer = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_breast_cancer.F, len(d_breast_cancer.history))
+print(fitness(X_test, Y_test, target_map, K, d_breast_cancer.X))
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_diabetes())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = GA(pop_size=50)
+d_diabetes = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_diabetes.F, len(d_diabetes.history))
+print(fitness(X_test, Y_test, target_map, K, d_diabetes.X))
+
+
+file = open('data/GA.dat', 'wb')
+d = {"wine": d_wine, "breast_cancer": d_breast_cancer, "diabetes": d_diabetes}
+pickle.dump(d, file)
+file.close()
+
+
+"""# Comparison
+
+# ES
+"""
+
+
+from pymoo.optimize import minimize
+from pymoo.algorithms.soo.nonconvex.es import ES
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_wine())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = ES(pop_size=50)
+d_wine = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_wine.F, len(d_wine.history))
+print(fitness(X_test, Y_test, target_map, K, d_wine.X))
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_breast_cancer())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = ES(pop_size=50)
+d_breast_cancer = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_breast_cancer.F, len(d_breast_cancer.history))
+print(fitness(X_test, Y_test, target_map, K, d_breast_cancer.X))
+
+
+X, X_test, Y, Y_test, target_map, mu, sigma = prepare_dataset(datasets.load_diabetes())
+K = len(target_map)
+bee_size = K * len(X[0])
+p = problem()
+algorithm = ES(pop_size=50)
+d_diabetes = minimize(p, algorithm, seed=1, verbose=False, save_history=True)
+print(d_diabetes.F, len(d_diabetes.history))
+print(fitness(X_test, Y_test, target_map, K, d_diabetes.X))
+
+
+file = open('data/ES.dat', 'wb')
+d = {"wine": d_wine, "breast_cancer": d_breast_cancer, "diabetes": d_diabetes}
+pickle.dump(d, file)
+file.close()
---
+++ t2/report.md
@@ -0,0 +1,134 @@
+# Tarefa Computacional 2
+Projeto, implementação do algoritmo Artificial Bee Colony (ABC) para clusterização de dados
+
+> Leandro Furlam Turi
+
+
+## 1. Implementação
+### 1.1. Aspectos gerais
+Toda a implementação da metaheurística foi realizada utilizando apenas pacotes-padrão da linguagem Python, bem como o pacote *Numpy*. As bases de dados e a divisão da base de treino e teste foram realizadas através do pacote *Sckit-learn*.
+
+### 1.2. Bases de dados
+Foram utilizadas as bases de dados *Breast Cancer*, *Diabetes* e *Wine*, todas disponíveis pelo *Sckit-learn*. 
+A base de dados original recebeu os tratamentos normalização z-score para as bases de entrada.
+
+### 1.3. Algoritmo Evolutivo: Artificial Bee Colony
+A implementação realizada baseou-se nos trabalhos de [Shams K. Nseef et. al. [2016]](https://www.sciencedirect.com/science/article/abs/pii/S0950705116300363).
+
+O algoritmo foi proposto por [Karaboga, 2008](https://abc.erciyes.edu.tr/pub/tr06_2005.pdf) como um algoritmo de inteligência de enxame inspirado na natureza baseado na observação do comportamento de forrageamento das abelhas. No ABC, há um conjunto de fontes de alimento e um conjunto de abelhas. A qualidade das fontes de alimento é baseada na quantidade de néctar que elas contêm. As abelhas procuram e colaboram umas com as outras, buscando melhores fontes de alimento.  
+Para resolver um problema de otimização usando o ABC, as fontes de alimento representam a população de soluções para um determinado problema e as abelhas são categorizadas em três tipos: *batedoras*, *funcionárias* e *observadoras*. A quantidade de néctar corresponde à qualidade (função objetivo) do problema que está sendo tratado. Os três tipos de abelhas trabalham juntos de forma iterativa para melhorar a qualidade da população de soluções (fontes de alimento). O pseudo-código de um ABC básico é mostrado a seguir. O ABC primeiro define os parâmetros principais, inicializa a população de soluções e depois as avalia. Em seguida, o loop principal é executado na tentativa de resolver o problema de otimização dado chamando as abelhas funcionárias, abelhas observadoras e abelhas batedoras até que a condição de parada seja satisfeita.
+
+Algoritmo: Pseudo-código do ABC
+```
+Passo 1: Inicializar parâmetros
+Passo 2: Inicializar a população de soluções
+Passo 3: Avaliar a população de soluções
+enquanto a condição de término não for atendida faça
+    Passo 4: Etapa das abelhas funcionárias
+    Passo 5: Etapa das abelhas observadoras
+    Passo 6: Etapa das abelhas batedoras
+fim-enquanto
+```
+
+* Passo 1: Nesta etapa são inicializados os principais parâmetros do ABC. Estes incluem: o número máximo de iterações (`max_it`) que representa a condição de parada do ABC, o número de soluções ou tamanho da população (`PS`) que representa quantas soluções serão geradas, o número total de abelhas (Sbees) que é definido como o dobro do tamanho de Ps, onde metade delas são abelhas empregadas e a outra metade são abelhas observadoras, o parâmetro limite (`Lit`), que é usado para determinar se a solução deve ser substituída por uma nova.
+
+
+* Passo 2: Um conjunto de soluções com tamanho igual a `PS` é gerado aleatoriamente da seguinte forma:
+
+`x_i = x_min + rand[0,1](x_max - x_min)`
+
+onde `x_max` e `x_min` são os limites máximo e mínimo da variável de decisão `x`.
+
+
+* Passo 3: Cálculo da aptidão das soluções. É importante ter este passo, visto que uma solução pode ser avaliada diversas vezes em uma mesma iteração.
+
+
+* Passo 4: Etapa das abelhas funcionárias. Cada abelha funcionária é enviada para uma fonte de alimento (solução). Sua principal função é explorar a vizinhança da solução atual, buscando uma solução melhorável. Uma solução de vizinhança, `v`, é criada modificando a `i`-ésima solução, `x`, como segue:
+
+`v_i = x_i + rand[-1,1](x_i - x_k)`
+
+onde `x_k` é gerado aleatoriamente na população. A solução de vizinhança gerada será substituída pela solução atual se tiver melhor aptidão.
+
+
+* Passo 5: Etapa das abelhas observadoras. As abelhas observadoras procuram melhorar a população atual de soluções explorando sua vizinhança usando a mesma que a abelha funcionária. A diferença é que as abelhas observadoras selecionam as soluções probabilisticamente com base em seus valores de aptidão da seguinte forma:
+
+`p_i = fitness / sum_j=1:PS(fitness_j)`
+
+Ou seja, a solução com maior aptidão tem maior chance de ser selecionada (ou seja, seleção da roleta). As abelhas observadoras utilizam um mecanismo de seleção guloso, onde a melhor solução em termos de aptidão é selecionada.
+
+
+* Passo 6: Etapa das abelhas batedoras
+Esta etapa é ativada se as abelhas funcionárias e observadoras não puderem melhorar a solução atual por um número de iterações consecutivas definido pelo parâmetro limite, `Lit`. Isso indica que a solução atual não é boa o suficiente para pesquisar sua vizinhança e deve ser descartada. Nesse caso, a abelha exploradora gerará uma nova solução usando a Equação descrita no Passo 2 para substituir a descartada. Isso pode ajudar o ABC a escapar de um ótimo local e explorar uma área diferente do espaço de busca.
+
+
+### 1.4. Modelagem do problema
+#### 1.4.5. Parâmetros
+- PS = 50  
+- Lit = 30  
+- max_it = 500  
+
+#### 1.4.2. Indivíduo (abelha)
+Cada abelha representa `K` centróides em `R^n_var`, onde `K` é o número de clusters e `n_var` é a dimensão do espaço de variáveis de entrada. Para simplificar os cômputos, todos os pontos foram rearranjados em um único vetor `v = {a_1, a_2, a_3, ..., a_n_var, b_1, b_2, b_3, ..., b_n_var, ..., k_1, k_2, k_3, k_n_var}`.
+
+
+#### 1.4.3. Função Fitness
+Cada um dos clusters descritos anteriormente é mapeado diretamente a uma das classes da base de dados. É calculada a norma euclideana entre cada observação da base de dados e o cluster de interesse, onde a classe mapeada do cluster mais próximo será a da observação.  
+Para avaliar a eficácia do método, é calculada a taxa de acertos em relação ao número total de observações.
+
+
+## 2. Resultados obtidos
+Os resultados obtidos são apresentados na Tabela a seguir:
+Base de dados | fitness treino | fitness teste | iter | time
+--- | --- | --- | --- | ---
+Wine | 0.766917 | 0.733333 | 499 | 1min 33s
+Breast Cancer | 0.924882 | 0.930069 | 499 | 4min 07s
+Diabetes | + | + | 499 | 2h 48min 35s
+
++: não houve convergência.
+
+Destaca-se a boa solução encontrada nas bases Wine e Breast Cancer, cuja quantidade de grupos é 3 e 2, respectivamente, com um tempo computacional satisfatório.  
+Com a Base Diabetes, possuido 214 grupos, sequer foi possível encontrar uma solução minimamente aceitável, mesmo com um tempo computacional viável.
+
+
+## 2.1. Comparação com as heurísticas GA e ES
+As heurísticas GA e ES utilizanas nesta comparação são as provenientes do pacote *pymoo*, com configuração-padrão, a não ser o tamanho populacional, mantido o mesmo do ABC (50 indivíduos).
+
+Destaca-se que apenas houve comparação em relação à qualidade da solição, visto que a implementação deste pacote é mais otimizada.
+
+
+Wine
+Herística | fitness treino | fitness teste | iter
+--- | --- | --- | ---
+ABC | 0.766917 | 0.733333 | 499
+GA  | 0.684210 | 0.711111 | 47
+ES  | 0.684210 | 0.733333 | 77
+
+Breast Cancer 
+Herística | fitness treino | fitness teste | iter
+--- | --- | --- | ---
+ABC | 0.924882 | 0.930069 | 499
+GA  | 0.903755 | 0.944055 | 75
+ES  | 0.913145 | 0.944055 | 71
+
+Diabetes
+Herística | fitness treino | fitness teste | iter
+--- | --- | --- | ---
+ABC | + | + | 499
+GA  | + | + | 51
+ES  | + | + | 38
+
+A convergência em cada base de dados é apresentada nas Figuras a seguir
+
+Wine  
+![wine](data/wine.png)
+
+Breast Cancer  
+![breast_cancer](data/breast_cancer.png)
+
+Diabetes   
+![diabetes](data/diabetes.png)
+
+
+## 3. Conclusão
+Dada sua natureza semelhante às demais heurísticas Genéticas estudadas, o ABC demonstrou ser um bom candidato à classificação de dados quando o número de grupos é pequeno. Sua estrutura simples, porém poderosa, permitiu encontrar uma boa solução nas bases Wine e Breast Cancer. Caso este que não ocorreu para base Diabetes.  
+Destaca-se que sua implementação computacional pode ser otimizada e paralelizada, de forma a diminuir o tempo de execução.
